c8947874ca9e296c237f7eddeb93ec9a2d809999
spselenim <pawal.satish@gmail.com>
2020-08-26 22:37:44 +0530

    3

--- myTest1/src/test/java/com/stc/test/myTest1/MyTest1.java
+++ myTest1/src/test/java/com/stc/test/myTest1/MyTest1.java
@@ -4,7 +4,7 @@ public class MyTest1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("First-11-21-A");
+System.out.println("First-11-21-A1");
 	}
 
 }
